f78823874fe0c28958c1c1aa24769cb24576f1a6
WanderingPenwing <nicolas.pinson31@gmail.com>
2024-03-13 11:52:22 +0100

    layout example

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import kivy
 kivy.require('1.0.7')
 
 from kivy.app import App
+from kivy.uix.widget import Widget
 
+class FileLayout(Widget):
+    pass
+
+class FileLayoutTest(Widget):
+    pass
 
 class YubabaApp(App): #load the yubaba.kv file
-    pass
+	pass
 
 
 if __name__ == '__main__':
-    YubabaApp().run()
+	YubabaApp().run()
