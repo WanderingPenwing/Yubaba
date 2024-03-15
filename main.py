63c6854fa08ca4f06b9c23318b62c727e570fa2e
Thom125 <thomas.mestressat@gmail.com>
2024-03-15 18:15:13 +0100

    button de ses morts

--- main.py
+++ main.py
@@ -5,19 +5,23 @@ import sys
 
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 
 def handle_file_open(file_path):
     # Your code to handle the opened file
 	print("opened with yubaba : " + file_path)
 
-class FileLayout(Widget):
-	pass
+class MainLayout(BoxLayout):
+	def open_files (self):
+		print ("open")
 
-class FileLayoutTest(Widget):
-	pass
-
+			
 class YubabaApp(App): #load the yubaba.kv file
+	def build(self):
+		return MainLayout()
 	pass
+    
 
 
 if __name__ == '__main__':
@@ -26,3 +30,4 @@ if __name__ == '__main__':
 		handle_file_open(file_path)
 	else:
 		YubabaApp().run()
+		
