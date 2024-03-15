b04d2c29fd6729918784b34a99cf6362ccec171a
WanderingPenwing <nicolas.pinson31@gmail.com>
2024-03-15 17:06:17 +0100

    preparation to handle file opening with yubaba

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 #Ceci est un test pour le push
 import kivy
 kivy.require('1.0.7')
+import sys
 
 from kivy.app import App
 from kivy.uix.widget import Widget
 
+def handle_file_open(file_path):
+    # Your code to handle the opened file
+	print("opened with yubaba : " + file_path)
+
 class FileLayout(Widget):
-    pass
+	pass
 
 class FileLayoutTest(Widget):
-    pass
+	pass
 
 class YubabaApp(App): #load the yubaba.kv file
 	pass
 
 
 if __name__ == '__main__':
-	YubabaApp().run()
+	if len(sys.argv) > 1:
+		file_path = sys.argv[1]
+		handle_file_open(file_path)
+	else:
+		YubabaApp().run()
