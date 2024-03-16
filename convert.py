cb58befa994ade5e9ac6f6ab333f326eddaf14fc
eliott.jouvin <eliott.jouvin@2025.icam.fr>
2024-03-16 01:34:39 +0100

    Exploseur d'adresses terminé voir func find_file_name in convert.py

--- convert.py
+++ convert.py
@@ -7,19 +7,23 @@ import ffmpeg
 #     .run()
 # )
 
-type_conv = {
-    'IMAGE' : ['jpeg','png'],
+TYPE = {
+    'IMAGE' : ['jpeg','jpg','png','webp'],
 	'VIDEO' : ['mp4'],
 	'AUDIO' : ['mp3','wav']
 }
+
+
 def find_file_name(input_path):
 	name = ''
 	ext = ''
+	parent = ''
+
 	dot = False
 	slash = False
 	for _ in range(-1,-len(input_path)-1,-1):
 		L = input_path[_]
-		if L == '.':
+		if L == '.' and not dot:
 			dot = True
 			continue
 		if L == '/':
@@ -27,29 +31,39 @@ def find_file_name(input_path):
 		
 		if not dot:
 			ext += L
-		
-		if dot and not slash:
+		elif dot and not slash:
 			name += L
-	return name[::-1], ext[::-1]
+		elif slash:
+			parent += L
+	return parent [::-1], name[::-1], ext[::-1]
 
-def convert(input_path, output_path, ex_from='', ex_to='') :
-	
+
+def convert(input_path, ex_to=str,  output_path='') :
 	file_type = None
-	for key in type_conv:
-		for ext in type_conv[key]:
+	parent, file, ex_from = find_file_name(input_path)
+	
+	if output_path == '':
+		output_path = parent + file + "." + ex_to
+	else: 
+		output_path += file + "." + ex_to
+	
+	print(output_path)
+
+	for KEY in TYPE:
+		for ext in TYPE[KEY]:
 			if ext == ex_from:
-				print('You are trying to convert a '+ key)
-				file_type = key
+				print('You are trying to convert a '+ KEY)
+				file_type = KEY
 				break
 		if file_type != None:
 			break
-	
-	print(find_file_name(input_path))
 
-	ffmpeg.input('input_path')
-#     .output('')
-#     .run()
-
+	(
+	ffmpeg
+	.input(input_path)
+	.output(output_path)
+	.run()
+	)
 
 if __name__ == '__main__':
-	convert('kijuytrezhgfd/test.webp', 'images/test.jpg')
+	convert('audios/Macroblank - VANDAL CLUB - 01 vandal 1.mp3', 'wav')
