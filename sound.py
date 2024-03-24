43cfde4f8cae20c7fd96e925f8e20e94843311a9
Thom125 <thomas.mestressat@gmail.com>
2024-03-24 18:27:38 +0100

    l audio

--- sound.py
+++ sound.py
@@ -1,22 +1,12 @@
-import pydub
 import ffmpeg
-from pydub import AudioSegment
 
 class Sound_convert ():
     def __init__(self, output, input):
         self.output = output
         self.input = input
     
-    def export (self, output_format):
-        input_format = self.input.split('.')
-        if self.output == '':
-            self.output = str(self.input[0]) + output_format
-        input_format = input_format[len(input_format)-1]
-        sound = AudioSegment.from_file(self.input, format=input_format)
-        sound.export(self.output, format=output_format)
+    def convert (self):
+        ffmpeg.input(self.input).output(self.output).run()
 
-    def ffmaudio (self):
-        ffmpeg.input("C:/Users/thoma/OneDrive/Docs/ICAM/Hackathon/Yubaba/Macroblank.mp3").output('C:/Users/thoma/OneDrive/Docs/ICAM/Hackathon/Yubaba/Macroblank.wav').run()
 if __name__ == '__main__':
     a = Sound_convert('', 'Macroblank.mp3')
-    a.ffmaudio()
