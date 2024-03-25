0d114d25a01cfe3f90aabd16eaf01f6761a45798
Paul <paul.lagraula@2025.icam.fr>
2024-03-25 17:39:30 +0100

    tets

--- image.py
+++ image.py
@@ -1,22 +1,21 @@
 import ffmpeg
 
-def audio_convert (input_list, output_folder, output_extension):
-    for input_file in input_list :
+def image_convert(input_list, output_folder, output_extension):
+    for input_file in input_list:
         input_file = input_file.replace("\\", "/")
         input_name = input_file.split('.')
         
         input_path = input_name[0].split("/")
         
-        if output_folder != "" and output_folder[-1] != "/" :
+        if output_folder != "" and output_folder[-1] != "/":
             output_folder += "/"
         
         output_file = output_folder + input_path[-1] + "." + output_extension
 
         print("------------------------------------")
-        print(input_file, output_file)
-        ffmpeg.input(input_file).output(output_file).run()
+        print(input_file, " -> ", output_file)
+        ffmpeg.input(input_file).output(output_file, vframes=1).run()
 
 if __name__ == '__main__':
-    audio_convert(['audios/Macroblank.mp3'], '', 'wav')
-    audio_convert(['Macroblank.wav'], 'audios', 'ogg')
-    
+    image_convert(['D:\Docs\@PAUL\ICAM\I4\Hackhaton\Yubaba\images\crazy squirrel.jpg'], '', 'png')
+    image_convert(['D:\Docs\@PAUL\ICAM\I4\Hackhaton\Yubaba\images\crazy squirrel.png'], 'converted_images', 'jpg')
